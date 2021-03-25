9fc01a41156d9b710cf6841968d25ff44d88a6d8
Devansh-Trivedi <tridevansh.160601@gmail.com>
2021-03-25 16:25:17 +0530

    changes

--- contact-form.php
+++ contact-form.php
@@ -5,12 +5,12 @@ $Email = $_REQUEST['email'];
 $Phone = $_REQUEST['phone'];
 $Subject = $_REQUEST['subject'];
 $Message = $_REQUEST['message'];
-$header = "From: devansh-trivedi.000webhostapp.com/";
+$header = $Email;
 if(empty($Name) && empty($Email) && empty($Subject) && empty($Message)){
     echo ('Please fill all the fields');
 }
 else{
-    mail("Sales@insightskv.com", "Message From Personal website - ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
+    mail("Sales@insightskv.com", "Message From InsightsKV.com - ".$Subject,"Name: ".$Name."Phone: " .$Phone." Message: " .$Message." Email: ".$Email, $header);
     echo "<script type='text/javascript'> alert('Your Message sent successfully');
             window.history.go(-1);
           </script>";
